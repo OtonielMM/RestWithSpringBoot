e937a13a9dc04a9c52120b5ace9bc3a469981dec
Equipe <equipe@MacBook-Air-de-Giulianna.local>
2020-01-16 18:01:58 -0300

    Migrations

---
+++ 05-rest-with-spring-boot-eclipse - MIGRATIONS/src/main/java/br/com/tom/controller/PersonController.java
@@ -0,0 +1,67 @@
+package br.com.tom.controller;
+
+// https://github.com/leandrocgsi/SpringBootPlayground
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tom.data.vo.PersonVO;
+import br.com.tom.data.vo.v2.PersonVOv2;
+import br.com.tom.services.PersonServices;
+
+
+@RestController
+@RequestMapping("/person")
+
+public class PersonController {
+	
+	@Autowired
+	private PersonServices services;
+	
+	@GetMapping("/{Id}")
+	public PersonVO findById(@PathVariable("Id") Long Id) {
+
+	return services.findById(Id);
+	}
+	
+	@GetMapping
+	public List<PersonVO> findAll() {
+
+	return services.findAll();
+	}
+	
+	
+	@PostMapping
+	public PersonVO create(@RequestBody PersonVO person){
+	    return services.create(person);
+	}
+   
+	@PostMapping("/v2")
+	public PersonVOv2 createv2(@RequestBody PersonVOv2 person){
+	    return services.createv2(person);
+	}
+     
+		
+	@PutMapping("/{Id}")
+	public PersonVO update(@RequestBody PersonVO person) {
+
+	return services.update(person);
+	}
+	
+	@DeleteMapping("/{Id}")
+	public ResponseEntity<?> delete(@PathVariable("Id") Long Id) {
+		
+		services.delete(Id);
+		return ResponseEntity.ok().build();
+	}
+}
---
+++ 05-rest-with-spring-boot-eclipse - MIGRATIONS/src/main/java/br/com/tom/services/PersonServices.java
@@ -0,0 +1,66 @@
+package br.com.tom.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import br.com.tom.converter.DozerConverter;
+import br.com.tom.converter.custom.PersonConverter;
+import br.com.tom.data.model.Person;
+import br.com.tom.data.vo.PersonVO;
+import br.com.tom.data.vo.v2.PersonVOv2;
+import br.com.tom.exception.ResourceNotFoundException;
+import br.com.tom.repository.PersonRepository;
+
+@Service // Responsável por realizar a injecão de dependencia
+
+public class PersonServices {
+    
+    @Autowired
+    PersonRepository repository;
+    
+    public PersonVO create(PersonVO person) {
+    	Person entity = DozerConverter.parseObject(person, Person.class);
+    	PersonVO vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+    	return vo;
+    }
+    
+    
+    @Autowired
+    PersonConverter converter;
+    public PersonVOv2 createv2(PersonVOv2 person) {
+    	Person entity = converter.converterVoToEntity(person);
+    	PersonVOv2 vo = converter.converterEntityToVo(repository.save(entity));
+    	return vo;
+    }
+ 
+    public PersonVO findById(Long id) {
+    	Person entity = repository.findById(id)
+        		.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return DozerConverter.parseObject(entity, PersonVO.class);
+    }
+ 
+    public List<PersonVO> findAll() {
+    	return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+    }
+     
+    public PersonVO update(PersonVO person) {
+    	Person entity = repository.findById(person.getId())
+                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+		entity.setAddress(person.getAddress());
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setGender(person.getGender());
+		PersonVO vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+    	return vo;
+    }
+ 
+    public void delete(Long id) {
+    	Person person = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		repository.delete(person);
+    }
+}
